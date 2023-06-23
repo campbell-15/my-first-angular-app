b7f4abc18c18726bd7a567eb0d45c6f09e634367
Campbell Davies-Webb <campbelldavies-webb@Campbells-MacBook-Air.local>
2023-06-23 13:31:19 +0200

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-servers',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+    serverID: number = 10;
+    serverStatus: string ='offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+}
