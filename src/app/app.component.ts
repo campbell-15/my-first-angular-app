f6eb6ff303f5ec137305f092631211bfa90e76da
Campbell Davies-Webb <campbelldavies-webb@Campbells-MacBook-Air.local>
2023-06-24 19:50:25 +0200

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+// }
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +15,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
 }
